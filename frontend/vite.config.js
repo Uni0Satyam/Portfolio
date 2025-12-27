97487263489bd9c518a3a651aee44c8002949e29
Satyam Kushwaha <satyamkushwaha10c@gmail.com>
2025-12-27 18:54:04 +0530

    Change API proxy to production URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss(),],server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://portfolio-u0tl.onrender.com",
     },
   },
 })
